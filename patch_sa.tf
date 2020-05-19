380b8efb9862a6d530ff0e2f36494f72ba2d5a3f
Mohammad Karimi <mohammadkarimi@MacBook-Pro.local>
2020-05-19 12:17:11 -0500

    extra brackets been deleted

--- patch_sa.tf
+++ patch_sa.tf
@@ -2,11 +2,10 @@ resource “null_resource” “login” {
   triggers = {
     always_run = “${timestamp()}”
   }
-}
   provisioner “local-exec” {
     command = “kubectl patch serviceaccount default -p ‘{“imagePullSecrets”: [{“name”:  “nexus-creds”}]}’ -n prod”
   }
-}
+  
   provisioner “local-exec” {
     command = “kubectl patch serviceaccount default -p ‘{“imagePullSecrets”: [{“name”:  “nexus-creds”}]}’ -n qa”
   }
