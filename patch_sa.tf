2edc353419fe41d493404b9333c31107fd9af359
Mohammad Karimi <mohammadkarimi@MacBook-Pro.local>
2020-05-19 12:23:39 -0500

    new command added

--- patch_sa.tf
+++ patch_sa.tf
@@ -2,11 +2,17 @@ resource “null_resource” “login” {
   triggers = {
     always_run = “${timestamp()}”
   }
-  provisioner “local-exec” {
-    command = “kubectl patch serviceaccount default -p ‘{“imagePullSecrets”: [{“name”:  “nexus-creds”}]}’ -n prod”
+
+  provisioner "local-exec" {
+    command = "kubectl create namespace dev"
   }
+
   
-  provisioner “local-exec” {
-    command = “kubectl patch serviceaccount default -p ‘{“imagePullSecrets”: [{“name”:  “nexus-creds”}]}’ -n qa”
-  }
+ # provisioner “local-exec” {
+  #  command = “kubectl patch serviceaccount default -p ‘{“imagePullSecrets”: [{“name”:  “nexus-creds”}]}’ -n prod”
+  #}
+
+  #provisioner “local-exec” {
+   # command = “kubectl patch serviceaccount default -p ‘{“imagePullSecrets”: [{“name”:  “nexus-creds”}]}’ -n qa”
+  #}
 }
